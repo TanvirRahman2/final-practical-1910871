686f08a10ffe8491209510b698a4f49bb360a6a3
TanvirRahman2 <158074886+TanvirRahman2@users.noreply.github.com>
2024-04-26 11:47:45 +0600

    Update styles.css

---
+++ styles.css
@@ -0,0 +1,88 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: sandybrown;
+}
+
+header {
+  background-color:beige;
+  color:coral;
+  padding: 20px;
+  text-align: center;
+}
+
+main {
+  padding: 20px;
+}
+
+.search-container {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+#searchInput {
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px 0 0 4px;
+}
+
+#searchButton {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  border-radius: 0 4px 4px 0;
+  cursor: pointer;
+}
+
+.meal-card {
+  background-color: sandybrown;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 20px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.meal-card img {
+  max-width: 200px;
+  height: auto;
+  margin-bottom: 10px;
+}
+
+.show-all-btn {
+  display: block;
+  margin: 20px auto;
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #333;
+  color: white
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+footer {
+  background-color: #333;
+  color: white;
+  padding: 20px;
+  text-align: center;
+}
+
+@media (max-width: 768px) {
+  .meal-card {
+    flex-direction: row;
+    align-items: flex-start;
+  }
+
+  .meal-card img {
+    max-width: 100px;
+    margin-right: 10px;
+  }
+}
